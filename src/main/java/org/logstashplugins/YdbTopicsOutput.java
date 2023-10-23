856460e3d13a02238d18e970f3bc02336bb887e0
Mike Lukashev <boyskyfall@vk.com>
2023-10-23 18:20:15 +0300

    feat: add new features (like auth) and refactor code

--- src/main/java/org/logstashplugins/YdbTopicsOutput.java
+++ src/main/java/org/logstashplugins/YdbTopicsOutput.java
@@ -8,6 +8,10 @@ import co.elastic.logstash.api.Output;
 import co.elastic.logstash.api.PluginConfigSpec;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import tech.ydb.auth.AuthProvider;
+import tech.ydb.auth.NopAuthProvider;
+import tech.ydb.auth.TokenAuthProvider;
+import tech.ydb.auth.iam.CloudAuthHelper;
 import tech.ydb.core.grpc.GrpcTransport;
 import tech.ydb.topic.TopicClient;
 import tech.ydb.topic.write.AsyncWriter;
@@ -17,7 +21,6 @@ import tech.ydb.topic.settings.WriterSettings;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Iterator;
-import java.util.concurrent.CompletableFuture;
 
 
 /**
@@ -29,80 +32,61 @@ public class YdbTopicsOutput implements Output {
     public static final PluginConfigSpec<String> PREFIX_CONFIG = PluginConfigSpec.stringSetting("prefix", "");
 
     private final Logger logger = LoggerFactory.getLogger(YdbTopicsOutput.class);
-
     private final String topicPath;
     private final String producerId;
     private final String connectionString;
-    private final CompletableFuture<Void> initFuture;
-    private final Object initLock = new Object();
     private final String id;
-    private final String prefix;
     private volatile boolean stopped = false;
     private String currentMessage;
     private GrpcTransport transport;
     private TopicClient topicClient;
     private AsyncWriter asyncWriter;
-
+    private AuthProvider authProvider = NopAuthProvider.INSTANCE;
 
     public YdbTopicsOutput(String id, Configuration config, Context context) {
-        this.topicPath = config.get(PluginConfigSpec.stringSetting("topic_path"));
-        this.connectionString = config.get(PluginConfigSpec.stringSetting("connection_string"));
-
-        //Producer id передавать в конфиге ??
-        this.producerId = "your_producer_id";
-
-
         this.id = id;
-        this.prefix = config.get(PREFIX_CONFIG);
-        this.initFuture = initAsyncWriter();
+        topicPath = config.get(PluginConfigSpec.stringSetting("topic_path"));
+        connectionString = config.get(PluginConfigSpec.stringSetting("connection_string"));
+        producerId = config.get(PluginConfigSpec.stringSetting("producer_id"));
+
+        String accessToken = config.get(PluginConfigSpec.stringSetting("access_token"));
+        if (accessToken != null) {
+            authProvider = new TokenAuthProvider(accessToken);
+        } else {
+            String saKeyFile = config.get(PluginConfigSpec.stringSetting("service_account_key"));
+            if (saKeyFile != null) {
+                authProvider = CloudAuthHelper.getServiceAccountFileAuthProvider(saKeyFile);
+            }
+        }
     }
 
     @Override
     public void output(Collection<Event> events) {
-        try {
-            // проверка, что асинхронная операция инициализации писателя завершилась
-            if (initFuture != null) {
-                initFuture.get();
-            }
+        initializeTransport();
+        initializeWriter();
 
-            Iterator<Event> z = events.iterator();
-            while (z.hasNext() && !stopped) {
-                String s = prefix + z.next().getData();
-                sendMessage(s);
-            }
-
-        } catch (Exception e) {
-            logger.error("Error sending messages to YDB Topics: " + e.getMessage(), e);
+        Iterator<Event> z = events.iterator();
+        while (z.hasNext() && !stopped) {
+            String s = z.next().getData().toString();
+            sendMessage(s);
         }
     }
 
-    //чтобы избежать задержек, не блокируем основной поток инициализацией
-    public CompletableFuture<Void> initAsyncWriter() {
-        return CompletableFuture.runAsync(() -> {
-            try {
-                synchronized (initLock) {
-                    transport = GrpcTransport.forConnectionString(connectionString).build();
-                    topicClient = TopicClient.newClient(transport).build();
+    private void initializeTransport() {
+        transport = GrpcTransport.forConnectionString(connectionString)
+                .withAuthProvider(authProvider)
+                .build();
+        topicClient = TopicClient.newClient(transport).build();
+    }
 
-                    WriterSettings settings = WriterSettings.newBuilder()
-                            .setTopicPath(topicPath)
-                            .setProducerId(producerId)
-                            .setMessageGroupId(producerId)
-                            .build();
-
-                    asyncWriter = topicClient.createAsyncWriter(settings);
-
-                    asyncWriter.init()
-                            .thenRun(() -> logger.info("YDB Topics AsyncWriter initialized successfully"))
-                            .exceptionally(ex -> {
-                                logger.error("YDB Topics AsyncWriter initialization failed with ex: ", ex);
-                                return null;
-                            });
-                }
-            } catch (Exception e) {
-                logger.error("Failed to initialize YDB Topics Output: " + e.getMessage(), e);
-            }
-        });
+    private void initializeWriter() {
+        WriterSettings settings = WriterSettings.newBuilder()
+                .setTopicPath(topicPath)
+                .setProducerId(producerId)
+                .setMessageGroupId(producerId)
+                .build();
+        asyncWriter = topicClient.createAsyncWriter(settings);
+        asyncWriter.init().join();
     }
 
     private void sendMessage(String message) {
@@ -118,18 +102,14 @@ public class YdbTopicsOutput implements Output {
 
     @Override
     public void stop() {
-        if (asyncWriter != null) {
-            asyncWriter.shutdown();
-        }
-        if (transport != null) {
-            transport.close();
-        }
+        asyncWriter.shutdown();
+        transport.close();
         stopped = true;
     }
 
     @Override
     public void awaitStop() throws InterruptedException {
-        // Implement awaitStop if needed
+
     }
 
     @Override
