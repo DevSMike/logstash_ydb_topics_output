6f7ca1df6afe983c7178faef638c0abf329b37a0
Mike Lukashev <boyskyfall@vk.com>
2023-10-26 12:34:09 +0300

    feat: add new tests for JSON string

--- src/test/java/org/logstashplugins/YdbTopicsOutputTest.java
+++ src/test/java/org/logstashplugins/YdbTopicsOutputTest.java
@@ -12,29 +12,8 @@ import java.util.Map;
 import static junit.framework.TestCase.assertEquals;
 
 public class YdbTopicsOutputTest {
-
     private static final String CONNECTION_STRING = "grpc://localhost:2136?database=/local";
     private static final String TOPIC_PATH = "my-topic";
-
-    @Test
-    public void testMessageWritingBinary()  {
-        Map<String, Object> configValues = new HashMap<>();
-        configValues.put("topic_path", TOPIC_PATH);
-        configValues.put("connection_string", CONNECTION_STRING);
-        configValues.put("schema", "BINARY");
-
-        Configuration config = new ConfigurationImpl(configValues);
-
-        YdbTopicsOutput output = new YdbTopicsOutput("test", config, null);
-
-        CustomEvent event1 = new CustomEvent();
-        event1.setField("Hello, from YDB", null);
-
-        output.output(List.of(event1));
-        output.stop();
-
-        assertEquals(output.getCurrentMessage(), "{key1=Hello, from YDB}");
-    }
 
     @Test
     public void testMessageWritingJson()  {
@@ -42,17 +21,32 @@ public class YdbTopicsOutputTest {
         configValues.put("topic_path", TOPIC_PATH);
         configValues.put("connection_string", CONNECTION_STRING);
         configValues.put("schema", "JSON");
-
         Configuration config = new ConfigurationImpl(configValues);
-
         YdbTopicsOutput output = new YdbTopicsOutput("test", config, null);
-
         CustomEvent event1 = new CustomEvent();
-        event1.setField("value 1 2 3 4", null);
+        event1.setField("key1", "value 1 2 3 4");
 
         output.output(List.of(event1));
         output.stop();
 
         assertEquals(output.getCurrentMessage(), "{\"key1\":\"value 1 2 3 4\"}");
+    }
+
+    @Test
+    public void testMessageWritingJsonTwoObjects()  {
+        Map<String, Object> configValues = new HashMap<>();
+        configValues.put("topic_path", TOPIC_PATH);
+        configValues.put("connection_string", CONNECTION_STRING);
+        configValues.put("schema", "JSON");
+        Configuration config = new ConfigurationImpl(configValues);
+        YdbTopicsOutput output = new YdbTopicsOutput("test", config, null);
+        CustomEvent event1 = new CustomEvent();
+        event1.setField("key1", "value 1 2 3 4");
+        event1.setField("key2", null);
+
+        output.output(List.of(event1));
+        output.stop();
+
+        assertEquals(output.getCurrentMessage(), "{\"key1\":\"value 1 2 3 4\",\"key2\":null}");
     }
 }
